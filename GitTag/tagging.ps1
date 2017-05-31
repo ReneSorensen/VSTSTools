b53ddb2703ecd79128d5a1fff110cd8f28123057
Unknown <alekcarlsen@gmail.com>
2017-05-31 12:41:04 +0200

    fixed minor issues

--- GitTag/tagging.ps1
+++ GitTag/tagging.ps1
@@ -5,11 +5,10 @@ $workingDir = $env:SYSTEM_DEFAULTWORKINGDIRECTORY
 $currentBranch = $env:BUILD_SOURCEBRANCHNAME 
 $tag = Get-VstsInput -Name tag -Require
 
-if (!($authToken))
+if (!($env:SYSTEM_ACCESSTOKEN ))
 {
     throw ("OAuth token not found. Make sure to have 'Allow Scripts to Access OAuth Token' enabled in the build definition.
-			Also, give 'Project Collection Build Service' 'Contribute' and 'Create Tag' permissions - Cog -> Version Control -> {Select Repository/ies}
-	")
+			Also, give 'Project Collection Build Service' 'Contribute' and 'Create Tag' permissions - Cog -> Version Control -> {Select Repository/ies}")
 }
 
 # For more information on the VSTS Task SDK:
