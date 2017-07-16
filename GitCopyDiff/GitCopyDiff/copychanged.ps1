fd740fcd0deba3630750d6e415eb5f9ba0ff08cd
Unknown <alekcarlsen@gmail.com>
2017-07-17 00:52:43 +0200

    Created GitCopyDiff

---
+++ GitCopyDiff/GitCopyDiff/copychanged.ps1
@@ -0,0 +1,54 @@
+[CmdletBinding()]
+param()
+
+$workingDir = $env:SYSTEM_DEFAULTWORKINGDIRECTORY 
+$currentCommit = $env:BUILD_SOURCEVERSION
+$destination = Get-VstsInput -Name destination -Require
+$changeTypeInput = Get-VstsInput -Name changeType -Require
+$shouldFlattenInput = Get-VstsInput -Name flatten 
+$changeType = $changeTypeInput.split(",")
+[boolean]$shouldFlatten = [System.Convert]::ToBoolean($shouldFlattenInput)
+
+if (!($env:SYSTEM_ACCESSTOKEN ))
+{
+    throw ("OAuth token not found. Make sure to have 'Allow Scripts to Access OAuth Token' enabled in the build definition.")
+}
+
+# For more information on the VSTS Task SDK:
+# https://github.com/Microsoft/vsts-task-lib
+Trace-VstsEnteringInvocation $MyInvocation
+try {
+	Write-Verbose "Setting working directory to '$workingDir'."
+    Set-Location $workingDir
+	Write-Verbose "Current commit is $currentCommit"
+	
+	[System.Collections.ArrayList]$changes = @();
+	write-host "##[command]"git log -m -1 --name-status --pretty="format:" $currentCommit
+	git log -m -1 --name-status --pretty="format:" $currentCommit| foreach{
+    $item = $_.Split([char]0x0009);
+    if($changeType.Contains($item[0])){
+    $changes += ,$item[1];
+     }
+	}
+	
+	Write-Verbose "Changed files are:"
+	$changes | foreach { Write-Verbose $_ }
+	
+	IF($shouldFlatten)
+	{
+		$changes | foreach {
+			Copy-Item $_ -Destination $destination
+			}
+	}
+	else
+	{
+		$changes | foreach {
+		$destinationPath = join-path $destination $_;
+		New-Item -ItemType File -Path $destinationPath -Force | out-null
+		Copy-Item $_ -Destination $destinationPath -recurse -container;
+		}
+	}
+	
+} finally {
+    Trace-VstsLeavingInvocation $MyInvocation
+}
---
+++ GitCopyDiff/overview.md
@@ -0,0 +1,14 @@
+This extension includes the following tasks
+
+* Git Copy Changed Files.
+	
+## Git Copy Changed Files
+Copies files that have been added/modified/renamed/moved to a specified folder.
+
+### Prerequisites
+
+* Repository must be VSTS Git.
+* Allow scripts to access Oauth must be **Enabled**
+
+## Credits
+<div>Icons made by <a href="http://www.flaticon.com/authors/madebyoliver" title="Madebyoliver">Madebyoliver</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
